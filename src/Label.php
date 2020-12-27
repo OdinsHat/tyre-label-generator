df4c6267f9129e3eb0caca89d16ec71ca72e8c9e
Doug Bromley <doug@tintophat.com>
2020-12-27 13:48:52 +0000

    Making sure printed output of file names is lowercase but internally its uppercase

--- src/Label.php
+++ src/Label.php
@@ -80,7 +80,7 @@ class Label
             '<img src="%s/%s_%s.png" style="position:absolute;top:0;left:0;z-index:1" />',
             $this->images,
             $type,
-            $val
+            strtolower($val)
         );
     }
 
@@ -116,7 +116,7 @@ class Label
             '<img src="%s/%s_%s.png" class="tyre-label-overlay" />',
             $this->images_dir,
             $type,
-            $val
+            strtolower($val)
         );
     }
 
@@ -155,7 +155,7 @@ class Label
                 "%s/%s_%s.png",
                 $this->images_dir,
                 $type,
-                $val
+                strtolower($val)
             )
         );
 
